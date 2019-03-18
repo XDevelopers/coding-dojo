d7f20831911af4ce04f4de390df8e1b2ab2c43e9
Marcio Lourenco <xquema@gmail.com>
2019-03-18 16:59:40 -0300

    - rebase from develop;

---
+++ Coding.Dojo.Abtraction/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using Coding.Dojo.Abtraction.Enumerators;
+using System;
+
+namespace Coding.Dojo.Abtraction
+{
+    /// <summary>
+    /// https://my.api.mockaroo.com/codingdojo.json?key=beda91c0
+    /// </summary>
+    public class Cliente
+    {
+        public string NomeCompleto { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Email { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public string TelefoneCelular { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public Status Status { get; set; }
+
+        public bool ConsideradoAdulto { get; set; }
+    }
+}
---
+++ Coding.Dojo.Abtraction/Enumerators/Status.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Coding.Dojo.Abtraction.Enumerators
+{
+    public enum Status
+    {
+        [Description("Inativo")]
+        Indefinido = 0,
+
+        [Description("Ativo")]
+        PreCadastrado = 1
+    }
+}
